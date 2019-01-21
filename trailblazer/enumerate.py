f4608f0ca1cdb6e0b6ed850128882a7f6ef66137
Scott Piper <scott@summitroute.com>
2019-01-20 19:28:10 -0700

    Automated parameter generation; still needs some work

--- trailblazer/enumerate.py
+++ trailblazer/enumerate.py
@@ -1,14 +1,131 @@
 import re
+import json
+import string
 
 import boto3
 from botocore.exceptions import ClientError
 
+from xeger import Xeger
+
 from trailblazer import log
 from trailblazer.boto.util import botocore_config
 from trailblazer.boto.service import get_boto_functions, get_service_call_params, \
-									get_service_json_files, make_api_call, get_service_call_mutation
+									get_service_json_files, make_api_call
 from trailblazer.boto.sts import get_assume_role_session
 
+
+def snake_case_to_CamelCase(snake_str):
+    components = snake_str.split('_')
+    CamelCase = ''.join(x.title() for x in components)
+
+    # Fix special cases
+    CamelCase = CamelCase.replace('Id', 'ID')
+    CamelCase = CamelCase.replace('Ssh', 'SSH')
+    CamelCase = CamelCase.replace('Saml', 'SAML')
+    CamelCase = CamelCase.replace('Mfa', 'MFA')
+    CamelCase = CamelCase.replace('Ml', 'ML')
+
+    return CamelCase
+
+
+def get_value_for_shape(shape, shape_name, param_name, service_json):
+    if shape['type'] == 'list':
+        member_shape_name = shape['member']['shape']
+        member_shape = service_json['shapes'][member_shape_name]
+        return [get_value_for_shape(member_shape, member_shape_name, shape_name, service_json)]
+    elif shape['type'] == 'boolean':
+        return True
+    elif shape['type'] == 'double':
+        return 0.1
+    elif shape['type'] == 'float':
+        return 0.1
+    elif shape['type'] == 'integer':
+        return 1
+    elif shape['type'] == 'long':
+        return 1
+    elif shape['type'] == 'blob':
+        return 'blob'
+    elif shape['type'] == 'timestamp':
+        return '2019-01-19T18:39:31+00:00'
+    elif shape['type'] == 'structure':
+        response = {}
+        for member in shape.get('required', []):
+            member_shape_name = shape['members'][member]['shape']
+            member_shape = service_json['shapes'][member_shape_name]
+            response[member] = get_value_for_shape(member_shape, member_shape_name, shape_name, service_json)
+        return response
+    elif shape['type'] == 'map':
+        response = {}
+
+        member_shape_name = shape['key']['shape']
+        member_shape = service_json['shapes'][member_shape_name]
+        key_name = get_value_for_shape(member_shape, member_shape_name, shape_name, service_json)
+
+        member_shape_name = shape['value']['shape']
+        member_shape = service_json['shapes'][member_shape_name]
+        value_name = get_value_for_shape(member_shape, member_shape_name, shape_name, service_json)
+
+        return {key_name: value_name}
+
+    elif shape['type'] != 'string':
+        # TODO Should throw an error
+        log.error('Unexpected shape type \"{}\" for {}'.format(shape['type'], param_name))
+
+    if 'enum' in shape:
+        return shape['enum'][0]
+
+    if param_name == 'PolicyArn':
+        return 'arn:aws:iam::aws:policy/SecurityAudit'
+    elif param_name == 'PredictEndpoint':
+        return 'https://realtime.machinelearning.us-east-1.amazonaws.com' 
+    elif shape_name == 'arnType':
+        return 'arn:aws:iam::123456789012:user/David'
+    elif shape_name == 'accountAliasType':
+        return 'abc'
+    elif shape_name == 'OpenIDConnectProviderUrlType':
+        return 'http://www.example.com'
+    elif shape_name == 'DeletionTaskIdType':
+        return 'task/aws-service-role/lex.amazonaws.com/AWSServiceRoleForLexBots/ec720f7a-c0ba-4838-be33-f72e1873dd52'
+
+    # The shape must be a string and doesn't match a special case, so generate a string from the
+    # given regex pattern
+
+    pattern = '[a-z]+'
+    if 'pattern' in shape:
+        pattern = shape['pattern']
+
+    # Sometimes xeger crashes for the regex's, so we hard-code some values
+    if (pattern == "[\\p{L}\\p{Z}\\p{N}_.:/=+\\-@]+" or
+        pattern == "[\\p{L}\\p{Z}\\p{N}_.:\\/=+\\-@]*" or
+        pattern == "[\\p{L}\\p{Z}\\p{N}_.:/=+\\-@]*" or
+        pattern == "[\\p{L}\\p{Z}\\p{N}_.:\\/=+\\-@]*"):
+        return 'LZN'
+    if pattern == "^[\\w-.+]+@[\\w-.+]+$":
+        return "abc@example.com"
+    if pattern == "arn:aws.*:kinesis:.*:\\d{12}:stream/.*":
+        return "arn:aws:kinesis:us-east-1:123456789012:stream/example-stream-name"
+
+    max_length = shape.get('max', 100)
+    min_length = shape.get('min', 10) + 1
+    # In the case of TaskInput in sagemaker.render_ui_template the max length is 128,000 bytes, so we just keep it at 1024 so it's not too crazy.
+    if max_length > 1024 and 1024 > min_length:
+        max_length = 1024
+    x = Xeger(limit=max_length)
+    pattern = pattern.replace('.*', '[a-z]*')
+    log.debug('xeger using pattern: \"{}\" for {}'.format(pattern, shape_name))
+    value = x.xeger(pattern)
+
+    if value == '':
+        value = 'a'
+
+    # The reverse regex libraries don't seem to understand forward look-ahead tricks, so
+    # to get this to correct size, we'll grow it by concatenating it, then truncate it.
+    if len(value) < min_length:
+        value = value * min_length
+    if len(value) > max_length:
+        value = value[:max_length]
+
+    return value
 
 def enumerate_services(config, services, dry_run=False):
 
@@ -55,8 +172,12 @@ def enumerate_services(config, services, dry_run=False):
         # Get a list of params needed to make the serialization pass in botocore
         service_call_params = get_service_call_params(service_file_json[service])
 
+        service_json = json.load(open(service_file_json[service]))
+
         # Loop through all the functions and call them
         for function in functions_list:
+            # if not function[0].startswith('create_policy'):
+            #     continue
 
             # The service_file_json doesn't have underscores in names so let's remove them
             function_key = function[0].replace('_','')
@@ -88,9 +209,27 @@ def enumerate_services(config, services, dry_run=False):
                     try:
                         func_params = {}
 
-                        for param in params:
-                            # Set something because we have to
-                            func_params[param] = 'testparameter'
+                        # Automate parameter generation
+                        function_name_CamelCase = snake_case_to_CamelCase(function[0])
+                        function_json = service_json['operations'].get(function_name_CamelCase, {})
+                        if function_json == {}:
+                            log.error('Could not find {} in service json'.format(function_name_CamelCase))
+                            # TODO Raise this as an error
+                        elif 'input' in function_json:
+                            func_shape_name = function_json['input']['shape']
+                            func_shape = service_json['shapes'][func_shape_name]
+
+                            for required_param in func_shape.get('required', []):
+                                param_shape_name = func_shape['members'][required_param]['shape']
+
+                                param_shape = service_json['shapes'][param_shape_name]
+                                param_value = get_value_for_shape(param_shape, param_shape_name, required_param, service_json)
+
+                                func_params[required_param] = param_value
+
+                        # Fix one-off functions
+                        if function_name_CamelCase in ['ListServiceSpecificCredentials', 'ResetServiceSpecificCredential', 'ListServiceSpecificCredentials', 'DeleteServiceSpecificCredential', 'UpdateServiceSpecificCredential']:
+                            func_params['UserName'] = 'abc'
 
                         log.info('Calling {}.{} with params {} in {}'.format(service, new_func[0], func_params, region))
 
@@ -98,13 +237,14 @@ def enumerate_services(config, services, dry_run=False):
                         	make_api_call(service, new_func, region, func_params)
 
                     except ClientError as e:
-                        if "ValidationError" in str(e):
-                            log.error(e)
-                        else:
+                        if "AccessDenied" in str(e) or "UnauthorizedOperation" in str(e):
                             log.debug(e)
+                        else:
+                            log.error(e)
                     except boto3.exceptions.S3UploadFailedError as e:
                         log.debug(e)
                     except TypeError as e:
                         log.debug(e)
                     except KeyError as e:
-                        log.debug('Unknown Exception: {}.{} - {}'.format(service, new_func[0], e))
+                        log.error('KeyError Exception: {}.{} - {}'.format(service, new_func[0], e))
+                        #traceback.print_exc()
